b180fc6962797a09772a73c03e1935d4b69a3aeb
Gemma <gemma.percival@hotmail.co.uk>
2018-08-22 14:44:39 +0100

    added book columns

--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,5 +1,10 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+
 public class Book {
     private int id;
     private String title;
@@ -7,11 +12,60 @@ public class Book {
     private Boolean onLoan;
     private Borrower currentBorrower;
 
-    public Book(){
+    public Book(String title, String author, Boolean onLoan, Borrower currentBorrower){
         this.title = title;
         this.author = author;
         this.onLoan = onLoan;
         this.currentBorrower = currentBorrower;
-
     }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Column(name = "author")
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Column(name = "loan_status")
+    public Boolean getOnLoan() {
+        return onLoan;
+    }
+
+    public void setOnLoan(Boolean onLoan) {
+        this.onLoan = onLoan;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "borrower_id", nullable = false)
+    public Borrower getCurrentBorrower() {
+        return currentBorrower;
+    }
+
+    public void setCurrentBorrower(Borrower currentBorrower) {
+        this.currentBorrower = currentBorrower;
+    }
+
+
 }
