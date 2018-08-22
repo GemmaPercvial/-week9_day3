9c56d566894946ca2a93330fedd13cef5933f8f7
Vicky Jackson <vicky@digitalstuff.net>
2018-08-22 14:34:59 +0100

    Added Hibernate annotations to borrower

--- src/main/java/models/Borrower.java
+++ src/main/java/models/Borrower.java
@@ -1,14 +1,47 @@
 package models;
 
-import java.util.Set;
+import javax.persistence.*;
+import java.util.ArrayList;
 
+@Entity
+@Table(name="borrowers")
 public class Borrower {
     private int id;
     private String name;
-    private Set<Book> itemsBorrowed;
+    private ArrayList<Book> itemsBorrowed;
 
-    public Borrower(){
+    public Borrower(String name){
         this.name = name;
+        this.itemsBorrowed = new ArrayList<>();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name="items_borrowed")
+    public ArrayList<Book> getItemsBorrowed() {
+        return itemsBorrowed;
+    }
+
+    public void setItemsBorrowed(ArrayList<Book> itemsBorrowed) {
         this.itemsBorrowed = itemsBorrowed;
     }
 }
+
